ef08c668a1a266929ca17da13682045b3a204680
Larissa Stack <larissastack@gmail.com>
2023-04-11 09:19:22 -0700

    updated global arrays for questions on the quiz

---
+++ assets/script.js
@@ -0,0 +1,65 @@
+//global variables (array of questions, timer,)
+
+let quesArray = [];
+
+let ques1 = {
+   question: "What does 'HTML' stand for?",
+   options: "Hypertext Markup Language", "Hypertype Makeup Language", "Hypertone Markup Library", "Hypertext Makeup Library",
+   correct: "Hypertext Markup Language"
+};
+let ques2 = {
+    question: "What is a variable?",
+    options: "A name of a storage location.", "A script.", "An array of informtaion.", "A coding language.",
+    correct: "A name of a storage location."
+ };
+ let ques3 = {
+    question: "What character does an HTML tag use?",
+    options: " '' ", "[]", "<>", "=",
+    correct: "<>"
+ };
+ let ques4 = {
+    question: "What does the 'i' stand for in a for loop?",
+    options: "italics", "index", "identifier", "item",
+    correct: "index"
+ };
+ let ques5 = {
+    question: "What is the box on the outter edge of the box model in CSS?",
+    options: "Boarder", "Margin", "Padding", "Space",
+    correct: 
+ };
+ let ques6 = {
+    question: "In Javascript, how do you write the 'and'?",
+    options: "\\", "||", "==", "&&",
+    correct: "&&"
+ };
+ let ques7 = {
+    question: "What answers can you get from a boolean?",
+    options: "Numeric", "alphabet", "True/False", "Undefined",
+    correct: "True/False"
+ };
+// var questions = { <---- I am not sure if this one will work. 
+//     question: "What does 'HTML' stand for?",
+//     choices: ,
+//     correct: 
+//   }
+
+//variables to refrence DOM elements 
+
+/* function for starting quiz:
+    - hide the start-quiz section
+    - reveal the ques-container section
+    - start the timer
+    - call to function to get question
+*/
+
+/* function to get question from array of questions
+*/
+
+/*function to create the timer to go down*/
+
+/*function for checking answers
+
+/*Function to save high score
+
+/* Function to Retrieve local storage for high score
+
