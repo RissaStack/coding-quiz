352e84b7ffcaace6fe0fe498a44adbf78194ec02
Larissa Stack <larissastack@gmail.com>
2023-04-13 08:18:23 -0700

    added the function to start the quiz

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,49 @@
 //global variables: The let quesArray is used for all of the questions that will be asked during the quiz. They have objects in them to add all the options the user can chose from. 
-let quesArray = [];
+let quesArray = [
+   {
+      question: "What does 'HTML' stand for?",
+      options: ["Hypertext Markup Language", "Hypertype Makeup Language", "Hypertone Markup Library", "Hypertext Makeup Library"],
+      correct: "Hypertext Markup Language"
+   }, 
+   {
+      question: "What is a variable?",
+      options: ["A name of a storage location.", "A script.", "An array of informtaion.", "A coding language."],
+      correct: "A name of a storage location."
+   },
+   {
+      question: "What character does an HTML tag use?",
+      options: ["''", "[]", "<>", "="],
+      correct: "<>"
+   },
+   {
+      question: "What does the 'i' stand for in a for loop?",
+      options: ["italics", "index", "identifier", "item"],
+      correct: "index"
+   },
+   {
+      question: "What is the box on the outter edge of the box model in CSS?",
+      options: ["Boarder", "Margin", "Padding", "Space"],
+      correct: "Margin"
+   },
+   {
+      question: "In Javascript, how do you write the 'and'?",
+      options: ["\\", "||", "==", "&&"],
+      correct: "&&"
+   },
+   {
+      question: "What answers can you get from a boolean?",
+      options: ["Numeric", "alphabet", "True/False", "Undefined"],
+      correct: "True/False"
+   }
+];
 
-let ques1 = {
-   question: "What does 'HTML' stand for?",
-   options: ["Hypertext Markup Language", "Hypertype Makeup Language", "Hypertone Markup Library", "Hypertext Makeup Library"],
-   correct: "Hypertext Markup Language"
-};
-let ques2 = {
-    question: "What is a variable?",
-    options: ["A name of a storage location.", "A script.", "An array of informtaion.", "A coding language."],
-    correct: "A name of a storage location."
- };
- let ques3 = {
-    question: "What character does an HTML tag use?",
-    options: ["''", "[]", "<>", "="],
-    correct: "<>"
- };
- let ques4 = {
-    question: "What does the 'i' stand for in a for loop?",
-    options: ["italics", "index", "identifier", "item"],
-    correct: "index"
- };
- let ques5 = {
-    question: "What is the box on the outter edge of the box model in CSS?",
-    options: ["Boarder", "Margin", "Padding", "Space"],
-    correct: "Margin"
- };
- let ques6 = {
-    question: "In Javascript, how do you write the 'and'?",
-    options: ["\\", "||", "==", "&&"],
-    correct: "&&"
- };
- let ques7 = {
-    question: "What answers can you get from a boolean?",
-    options: ["Numeric", "alphabet", "True/False", "Undefined"],
-    correct: "True/False"
- };
 
  var timeLeft = 120;
 
 //variables to refrence DOM elements
 var pageContainer = document.getElementById("page-container"); 
 
-var startQuiz = document.getElementById("start-quiz");
+var startQuizEl = document.getElementsByClassName("start-quiz");
 
 var quesContainer = document.getElementById("ques-container");
 
@@ -50,17 +51,31 @@ var question = document.getElementById("question");
 
 var choices = document.getElementById("choices");
 
+var paragraphEl = document.createElement("p")
+
+paragraphEl.textContent = " Welcome to the fullstack coding quiz where you can put your knowledge to the test by answering these questions."
+
+
+
 /* function for starting quiz:
     - hide the start-quiz section
     - reveal the ques-container section
     - start the timer
     - call to function to get question
 */
+function quesRendered(){
+   var questions = document.createElement("h2")
+   questions.textContent = quesArray[0].question
+   document.querySelector(".ques-container").append(questions)
+}
+
+function hideHomePage(){
+   document.querySelector("#home-page").style.display="none"
+};
 
 function startQuiz(){
-    hide startQuiz
-    show ques-container
-    <BUTTON ONCLICK="ShowAndHide()">Click me</BUTTON>
+   document.querySelector(".ques-container").style.display="block"
+   quesRendered()
 }
 
 /* function to get question from array of questions. Do I use a loop here?
@@ -68,9 +83,22 @@ function startQuiz(){
 
 /*function to create the timer to go down*/
 
-/*function for checking answers
-/*alert to show correct or incorrect?
+/*function for checking answers*/
+/*alert to show correct or incorrect?*/
 
-/*Function to save high score
+function answerCorrect() {
+   alert("CORRECT")
+};
 
-/* Function to Retrieve local storage for high score
+function answerWrong() {
+   alert("WRONG")
+};
+
+/*Function to save high score*/
+
+/* Function to Retrieve local storage for high score*/
+document.querySelector(".start-button").addEventListener("click", function() {
+   console.log("start buttone clicked")
+   hideHomePage();
+   startQuiz();
+})
